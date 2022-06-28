2f55c121df43c3f0b616bc8e5983d0d08a3d26d2
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-06-28 06:43:43 -0700

    To Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,7 @@ def uppercase(str):
     for i in range(len(str)):
         if str[i] >= 'a' and str[i] <= 'z':
             new_string = new_string + chr((ord(str[i]) - 32))
+            continue
         else:
             new_string = new_string + str[i]
-    print("{}".format(new_string));
+    print("{0}".format(new_string));
