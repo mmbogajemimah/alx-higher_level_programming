6928c7d64e416c73b84861751e611e997e226ef9
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-06 15:15:43 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary is {}:
-        return "None"
+    if a_dictionary is None:
+        return None
     else:
         maximum = 0
         win = ""
