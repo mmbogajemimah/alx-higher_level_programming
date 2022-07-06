79c684e6b8f5bada7866b66af081780a39aada84
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-06 15:16:53 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    else:
-        maximum = 0
-        win = ""
 
-        for key, value in a_dictionary.items():
-            if value > maximum:
-                maximum = value
-                win = key
-        return win
+    b_score = max(a_dictionary.values(), default=None)
+    for k, v in a_dictionary.items():
+        if v == b_score:
+            return k
