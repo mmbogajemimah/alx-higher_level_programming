1dc7ece21909bed6fa5baf3e8af349c548090e3e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-06 15:12:21 -0700

    advanced

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    args = sys.argv
-    l_args = len(args)
-    sum = 0
-
-    if l_args > 1:
-        for i in range(1, l_args):
-            sum += int(args[i])
-
-    print(sum)
+if __name__ == '
