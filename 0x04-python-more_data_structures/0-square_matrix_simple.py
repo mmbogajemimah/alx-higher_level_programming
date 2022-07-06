1c8f259c15f73a830be260379e3f237eae1939fa
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-06 07:36:53 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,8 @@
 def square_matrix_simple(matrix=[]):
     if matrix == [[]]:
         return None
-    rows = len(matrix)
     cols = len(matrix[0])
+    rows = len(matrix)
     new_matrix = [[0 for j in range(cols)] for i in range(rows)]
     for i in range(rows):
         for j in range(cols):
