6cfb3b302364b639cc9fb853999f738065d2d018
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-19 12:31:49 -0700

    Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class Square:
+    """
+    private instance attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
