5b500891c9a0b85eb6fb79afbc7f2665dd8b79b6
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-07-19 14:02:57 -0700

    Classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the square of the area
+        """
+        answer = self.__size ** 2
+        return answer
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the square of the area
+        """
+        answer = self.__size ** 2
+        return answer
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return None
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end='')
+            print()
