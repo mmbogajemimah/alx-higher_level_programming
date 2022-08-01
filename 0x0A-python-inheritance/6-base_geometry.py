c417bd409f9935fea2ce274e1a0d68ecbaa0fc14
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-08-01 15:42:09 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Base Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    Defines area method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
