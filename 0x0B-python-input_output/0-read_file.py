21430bfa6c15414fc5ae189cea6b4e54ffe976d3
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-08-03 00:44:20 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     reads a file in utf-8 encodinng
     """
     with open('my_file_0.txt', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        read_data = f.read()
+    print(read_data, end="")
