ef6f7e1031dd2e628fb8400a21a095e2416f7b6b
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-10-03 02:43:31 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let myVarOne = 'C is fun';
+let myVarTwo = 'Python is cool';
+let myVarThree = 'JavaScript is amazing';
+
+console.log(myVarOne);
+console.log(myVarTwo);
+console.log(myVarThree);
