159617ce96aa34530e5a6b14b80205ad01846662
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-10-03 14:17:33 -0700

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arrayNums = [];
+
+const lengthNums = process.argv.length;
+if (lengthNums === 2 || lengthNums === 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < lengthNums; i++) {
+    arrayNums.push(process.argv[i]);
+  }
+  arrayNums.sort().reverse();
+  console.log(arrayNums[1]);
+}
