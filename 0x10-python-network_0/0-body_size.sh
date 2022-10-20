8c04e0e2a01e70fe6ea8d5f80cfcdcd8d1666b16
jemimah mmboga <mmbogajemimah@gmail.com>
2022-10-20 13:38:32 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 #Takes URL, sends request to URL, displays content length
-curl -sI $1 | grep -i Content-Length | awk '{print $2}' 
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' 
