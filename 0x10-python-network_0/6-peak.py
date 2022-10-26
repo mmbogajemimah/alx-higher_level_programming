eff32986e7887c2241a177169ae90f97946e408a
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-10-26 13:52:11 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,20 @@
 
 
 def find_peak(list_of_integers):
-    if len(list_of_integers) <= 0:
+    if len(list_of_integers) == 0:
         return None
-    array = sorted(list_of_integers)
-    return array[-1]
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    mid = int(length / 2)
+    high = list_of_integers[mid]
+    if high > list_of_integers[mid - 1] and high > list_of_integers[mid + 1]:
+        return high
+    elif high < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
