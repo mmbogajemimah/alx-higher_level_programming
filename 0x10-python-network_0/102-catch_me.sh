8955f800a0e623dc8b2b710d6952e87d95ccfdd2
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-10-26 00:27:38 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request, causes the server to respond with a message
-curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" "$1"
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
