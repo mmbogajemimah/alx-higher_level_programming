ab1d147a8431d131011634e7d877563fec8772e3
jemimah mmboga <mmbogajemimah@gmail.com>
2022-11-15 20:16:51 +0000

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const fileUrl = process.argv[2];
 request
   .get(fileUrl)
   .on('response', function (response) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   });
